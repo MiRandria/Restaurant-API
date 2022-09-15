292d487865e6178546cc5404aca57800f7178413
Minosoa <103509226+MiRandria@users.noreply.github.com>
2022-09-16 00:14:14 +0300

    feat: add something

---
+++ orderInsert.sql
@@ -0,0 +1,5 @@
+insert into order values ('1','John','Doe','088743899','Lot 12','22-09-15','1');
+insert into order values ('2','Johnas','Doe','088è43899','Lot 13','22-09-15','2');
+insert into order values ('3','Sylvia','R','054567679','Lot 14','22-09-15','3');
+insert into order values ('4','Rakoto','Doe','025743899','Lot 15','22-09-15','4');
+insert into order values ('5','Rasoa','P','035468799','Lot 16','22-09-15','5');
