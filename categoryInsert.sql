15ad0f3ac4914675e9fd035be7ec3be8847d7a32
Minosoa <103509226+MiRandria@users.noreply.github.com>
2022-09-16 00:14:55 +0300

    feat: add someting

---
+++ categoryInsert.sql
@@ -0,0 +1,4 @@
+insert into category values ('1','Dessert');
+insert into category values ('2','Fast_Food');
+insert into category values ('3','Resistance');
+insert into category values ('4','Entrer');
