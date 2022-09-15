1c4555ed623068cfc9cd69712150c7625e8321c2
Minosoa <103509226+MiRandria@users.noreply.github.com>
2022-09-16 00:13:26 +0300

    feat: add something

---
+++ mealInsert.sql
@@ -0,0 +1,5 @@
+insert into meal values ('1','Glace','2000ar','1');
+insert into meal values ('2','Pizza','10000ar','2');
+insert into meal values ('3','Riz','6000ar','4');
+insert into meal values ('4','Jus','7000ar','1');
+insert into meal values ('5','Frite','2000ar','2');
